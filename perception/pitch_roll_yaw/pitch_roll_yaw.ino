d948cd072739e95c77619e4ba9c49a4fcf743127
jasminekamdar <35180562+jasminekamdar@users.noreply.github.com>
2019-10-23 15:45:59 -0400

    added pitch and pinch for the claw

--- perception/pitch_roll_yaw/pitch_roll_yaw.ino
+++ perception/pitch_roll_yaw/pitch_roll_yaw.ino
@@ -3,6 +3,17 @@
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_L3GD20_U.h>
 #include <Adafruit_9DOF.h>
+#include <Servo.h>
+
+Servo pitchservo;  // create servo object to control a servo
+Servo pinchservo;  // create servo object to control a servo
+Servo yawservo;  // create servo object to control a servo
+Servo rollservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int rollpos = 0;    // variable to store the servo position
+int pinchpos = 45; //starts pinch in open position
+int pitchpos = 90; //starts pitch in neutral position
 
 /* Assign a unique ID to the sensors */
 Adafruit_9DOF                dof   = Adafruit_9DOF();
@@ -24,10 +35,14 @@ int minPitchSensor = 90;
 void setup(void)
 {
   Serial.begin(115200);
-//  Serial.println(F("Adafruit 9 DOF Pitch/Roll/Heading /Example")); Serial.println("");
+  //  Serial.println(F("Adafruit 9 DOF Pitch/Roll/Heading /Example")); Serial.println("");
   pinMode(buttonPin, INPUT);
   /* Initialise the sensors */
   initSensors();
+  pitchservo.attach(11);  // attaches the pitch servo on pin 11 to the servo object
+  pinchservo.attach(9);  // attaches the pinch servo on pin 9 to the servo object
+  yawservo.attach(6);  // attaches the yaw servo on pin 6 to the servo object
+  rollservo.attach(10);  // attaches the roll servo on pin 10 to the servo object
 }
 
 /**************************************************************************/
@@ -55,29 +70,53 @@ void loop(void)
   if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
   {
     /* 'orientation' should have valid .heading data now */
-    Serial.print(F("Roll: "));
-    Serial.print(orientation.roll);
-    Serial.print(F("; "));
-    Serial.print(F("Pitch: "));
-    Serial.print(orientation.pitch);
-    Serial.print(F("; "));
-    Serial.print(F("Heading: "));
-    Serial.print(orientation.heading);
-    Serial.println(F("; "));
+    //    Serial.print(F("Roll: "));
+    //    Serial.print(orientation.roll);
+    //    Serial.print(F("; "));
+    //    Serial.print(F("Pitch: "));
+    //    Serial.print(orientation.pitch);
+    //    Serial.print(F("; "));
+    //    Serial.print(F("Heading: "));
+    //    Serial.print(orientation.heading);
+    //    Serial.println(F("; "));
   }
 
   buttonPressed = digitalRead(buttonPin);
-  Serial.println(buttonPressed);
+  // Serial.println(buttonPressed);
 
   // THINK
   int x = 100;
-  int y = map(x, 180,90,90,180);
-  Serial.println(y);
-  
+  int y = map(x, 180, 90, 90, 180);
+  //Serial.println(y);
+  int handpitch = orientation.pitch; //making float into an int
+  //Serial.println(handpitch);
+  if (handpitch > -150 && handpitch < 0) {
+    //Serial.println("less than 150");
+    handpitch = -150;
+  }
+  else if (handpitch > 180) {
+    //Serial.println("more than 180");
+    handpitch = 180;
+  }
+  else if (0 < handpitch && handpitch < 90) {
+    //Serial.println("between 0 and 90");
+    handpitch = 90;
+  }
 
+  int clawpitch;
+  if (-180 <= handpitch && handpitch <= -150) {
+    Serial.println("-180--150");
+    clawpitch = map(handpitch, -150, -180, 60, 90);
+  }
+  else if (90 <= handpitch && handpitch <= 180) {
+    Serial.println("90-180");
+    clawpitch = map(handpitch, 180, 90, 90, 180);
+  }
+  pitchpos=clawpitch;
+  Serial.println(clawpitch);
   // ACT
-
-  
+  closeclaw(buttonPressed);
+  pitchservo.write(pitchpos);
 }
 
 // HELPFUL FUNCTION
@@ -95,5 +134,15 @@ void initSensors()
     /* There was a problem detecting the LSM303 ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while (1);
+  }
+}
+void closeclaw(int buttonpress) {
+  if (buttonpress == 1) {
+    pinchpos = 0;
+    pinchservo.write(pinchpos);
+  }
+  if (buttonpress == 0) {
+    pinchpos = 45;
+    pinchservo.write(pinchpos);
   }
 }
