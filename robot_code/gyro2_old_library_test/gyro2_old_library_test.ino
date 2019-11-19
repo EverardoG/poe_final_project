4c3e9a3e7ddbc5bba8e9901abef06fa22b3cead1
Everardo Gonzalez <everardo.a.gonzalez@gmail.com>
2019-11-19 12:00:12 -0500

    added test code for using the old library with the new sensor

---
+++ robot_code/gyro2_old_library_test/gyro2_old_library_test.ino
@@ -0,0 +1,85 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_FXOS8700.h>
+#include <Adafruit_9DOF.h>
+
+/* Assign a unique ID to the sensors */
+Adafruit_9DOF                dof   = Adafruit_9DOF();
+using FX::Adafruit_FXOS8700;
+//using FX::Adafruit_FXAS21002C;
+
+Adafruit_FXOS8700 accelmag = Adafruit_FXOS8700(0x8700A, 0x8700B);
+//Adafruit_FXAS21002C gyro = Adafruit_FXAS21002C(0x0021002C);/
+
+//Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+//Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+
+/* Update this with the correct SLP for accurate altitude measurements */
+float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+
+/**************************************************************************/
+/*!
+    @brief  Initialises all the sensors used by this example
+*/
+/**************************************************************************/
+void initSensors()
+{
+  using FX::ACCEL_RANGE_4G;
+  if(!accelmag.begin(ACCEL_RANGE_4G))
+  {
+    /* There was a problem detecting the FXOS8700 ... check your connections */
+    Serial.println("Ooops, no FXOS8700 detected ... Check your wiring!");
+    while(1);
+  }
+}
+
+/**************************************************************************/
+/*!
+
+*/
+/**************************************************************************/
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println(F("Adafruit 9 DOF Pitch/Roll/Heading Example")); Serial.println("");
+  
+  /* Initialise the sensors */
+  initSensors();
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly check the roll/pitch/heading/altitude/temperature
+*/
+/**************************************************************************/
+void loop(void)
+{
+  sensors_event_t accel_event;
+  sensors_event_t mag_event;
+  sensors_vec_t   orientation;
+
+  /* Calculate pitch and roll from the raw accelerometer data */
+  accelmag.getEvent(&accel_event, &mag_event);
+  if (dof.accelGetOrientation(&accel_event, &orientation))
+  {
+    /* 'orientation' should have valid .roll and .pitch fields */
+    Serial.print(F("Roll: "));
+    Serial.print(orientation.roll);
+    Serial.print(F("; "));
+    Serial.print(F("Pitch: "));
+    Serial.print(orientation.pitch);
+    Serial.print(F("; "));
+  }
+  
+  /* Calculate the heading using the magnetometer */
+  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
+  {
+    /* 'orientation' should have valid .heading data now */
+    Serial.print(F("Heading: "));
+    Serial.print(orientation.heading);
+    Serial.print(F("; "));
+  }
+
+  Serial.println(F(""));
+  //delay(1000);
+}
